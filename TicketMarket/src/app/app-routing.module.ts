27e7202595940194497eb6ab4ad7c5f36c51c025
yankoBede <yanko.nikolov@bedegaming.com>
2020-02-09 16:05:44 +0200

    added initial components

--- TicketMarket/src/app/app-routing.module.ts
+++ TicketMarket/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
 
+const routes: Routes = [
+  {
+      path: '',
+      pathMatch: 'full',
+      component: HomeComponent
+  },
+  {
+    path : 'login',
+    component: LoginComponent,
+  },
+  {
+      path: '**',
+      component: NotFoundComponent
+  }
+];
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes);
--- TicketMarket/src/app/app.module.ts
+++ TicketMarket/src/app/app.module.ts
@@ -5,17 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CoreModule } from './core/core.module';
+import { CreateComponent } from './event/create/create.component';
+import { DetailComponent } from './event/detail/detail.component';
+import { ListComponent } from './event/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    HomeComponent,
+    NotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateComponent,
+    DetailComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
